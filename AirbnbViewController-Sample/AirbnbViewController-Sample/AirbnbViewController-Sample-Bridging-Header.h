9fa1a89e83fa74911f097a391cb3f5468a7c3cc1
Ahmed BOUCHFAA <abouchfaa@gmail.com>
2017-03-22 15:10:01 +0000

    removed bridging header

--- AirbnbViewController-Sample/AirbnbViewController-Sample/AirbnbViewController-Sample-Bridging-Header.h
+++
@@ -1,9 +0,0 @@
-//
-//  AirbnbViewController-Sample-Bridging-Header.h
-//  AirbnbViewController-Sample
-//
-//  Created by pixyzehn on 1/27/15.
-//  Copyright (c) 2015 pixyzehn. All rights reserved.
-//
-
-#import "AirbnbHelper.h"
